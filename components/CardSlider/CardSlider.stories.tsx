f75ac6e66d2953282994a5e8ffa29c787a30e4e5
MikleBr <barulin.ma@gmail.com>
2022-03-24 15:37:49 +0300

    Добавил настройка для нескольких колонок у слайдера

--- components/CardSlider/CardSlider.stories.tsx
+++ components/CardSlider/CardSlider.stories.tsx
@@ -1,73 +1,99 @@
-import React, {CSSProperties} from 'react';
+import React, { CSSProperties } from 'react';
 
-import {ComponentStory, ComponentMeta} from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {CardSlider} from './CardSlider';
-import {Box} from '../UI/Box/Box';
-import {Typography} from '../UI/Typography/Typography';
-import {Button} from '../UI/Button/Button';
+import { CardSlider } from './CardSlider';
+import { Box } from '../UI/Box/Box';
+import { Typography } from '../UI/Typography/Typography';
+import { Button } from '../UI/Button/Button';
 
 export default {
-    title: "components/CardSlider",
-    component: CardSlider,
+  title: 'components/CardSlider',
+  component: CardSlider,
 } as ComponentMeta<typeof CardSlider>;
 
 const Template: ComponentStory<typeof CardSlider> = function (args) {
-    return <CardSlider {...args} />;
+  return <CardSlider {...args} />;
 };
 
 export const DefaultState = Template.bind({});
 
 type CardProps = {
-    title: string;
-    image?: string;
+  title: string;
+  image?: string;
 };
 
-export function TemplateCard({title, image}: CardProps) {
-    const wrapperBoxSx: CSSProperties = {
-        width: '250px',
-        height: '156px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-        backgroundColor: '#EBEBEB;',
-        borderRadius: '8px',
-        overflow: 'hidden',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        padding: '26px 18px 13px 18px',
-        backgroundImage: image ? `url(${image})` : 'none',
-    };
-    return (
-        <Box sx={{...wrapperBoxSx}}>
-            <Typography variant="subtitle1">{title}</Typography>
-            <Button>подробнее</Button>
-        </Box>
-    );
+export function TemplateCard({ title, image }: CardProps) {
+  const wrapperBoxSx: CSSProperties = {
+    width: '250px',
+    height: '156px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    backgroundColor: '#EBEBEB;',
+    borderRadius: '8px',
+    overflow: 'hidden',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    padding: '26px 18px 13px 18px',
+    backgroundImage: image ? `url(${image})` : 'none',
+  };
+  return (
+    <Box sx={{ ...wrapperBoxSx }}>
+      <Typography variant="subtitle1">{title}</Typography>
+      <Button>подробнее</Button>
+    </Box>
+  );
 }
 
 DefaultState.args = {
-    title: 'Акции и скидки',
-    cardsList: [
-        <TemplateCard
-            title="test1"
-        />,
-        <TemplateCard
-            title="test2"
-        />,
-        <TemplateCard
-            image="https://images.unsplash.com/photo-1646309244219-9583e1341a00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            title="test3"
-        />,
-        <TemplateCard
-            image="https://images.unsplash.com/photo-1646309244219-9583e1341a00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            title="test4"
-        />,
-        <TemplateCard
-            image="https://images.unsplash.com/photo-1646309244219-9583e1341a00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            title="test5"
-        />,
-    ],
+  title: 'Акции и скидки',
+  cardsList: [
+    {
+      id: 1,
+      element: <TemplateCard title="test1" />,
+    },
+    {
+      id: 2,
+      element: <TemplateCard title="test2" />,
+    },
+    {
+      id: 3,
+      element: <TemplateCard title="test3" />,
+    },
+    {
+      id: 4,
+      element: <TemplateCard title="test4" />,
+    },
+    {
+      id: 5,
+      element: <TemplateCard title="test5" />,
+    },
+    {
+      id: 6,
+      element: <TemplateCard title="test6" />,
+    },
+    {
+      id: 7,
+      element: <TemplateCard title="test7" />,
+    },
+    {
+      id: 8,
+      element: <TemplateCard title="test8" />,
+    },
+    {
+      id: 9,
+      element: <TemplateCard title="test9" />,
+    },
+    {
+      id: 10,
+      element: <TemplateCard title="test10" />,
+    },
+    {
+      id: 11,
+      element: <TemplateCard title="test11" />,
+    },
+  ],
 };
--- components/CardSlider/CardSlider.tsx
+++ components/CardSlider/CardSlider.tsx
@@ -1,9 +1,9 @@
 import React, { CSSProperties, ReactNode, useState } from 'react';
 
-import { Button, ButtonGroup, Grid } from '@mui/material';
+import { Button, ButtonGroup, Stack } from '@mui/material';
 // eslint-disable-next-line
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
+import SwiperCore, { Grid, Navigation } from 'swiper';
 
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -11,45 +11,85 @@ import { Box } from '../UI/Box/Box';
 import { Container } from '../UI/Container/Container';
 import { Typography } from '../UI/Typography/Typography';
 
+import './CardSlider.module.scss';
+
+import 'swiper/css';
+import 'swiper/css/grid';
+
+type Card = {
+  id: number;
+  element: ReactNode;
+};
+
 type Props = {
-    title: string;
-    cardsList: ReactNode[];
+  title: string;
+  cardsList: Card[];
+  rows?: number;
+  cardHeight?: number;
+  spaceBetween?: number;
+  slidesPerView?: number;
 };
 
 const wrapperBoxSx: CSSProperties = {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 };
 
-export function CardSlider({ title, cardsList }: Props) {
-    const [slider, setSlider] = useState<SwiperCore | null>(null);
+export function CardSlider({
+  title,
+  cardsList,
+  rows = 1,
+  slidesPerView = 4,
+  spaceBetween = 10,
+  cardHeight = 195,
+}: Props) {
+  const [slider, setSlider] = useState<SwiperCore | null>(null);
 
-    return (
-        <Container sx={wrapperBoxSx}>
-            <Grid container alignItems="center" justifyContent="space-between">
-                <Grid item xs={6}>
-                    <Typography variant="h4">{title}</Typography>
-                </Grid>
-                <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <ButtonGroup>
-                        <Button onClick={() => slider?.slidePrev()}>
-                            <ArrowBackIosIcon />
-                        </Button>
-                        <Button onClick={() => slider?.slideNext()}>
-                            <ArrowForwardIosIcon />
-                        </Button>
-                    </ButtonGroup>
-                </Grid>
-            </Grid>
-            <Box sx={{ width: '100%', margin: '40px 0 0 0' }}>
-                <Swiper style={{padding: '10px'}} slidesPerView={4} onSwiper={setSlider}>
-                    {cardsList.map((card, i) => (
-                        <SwiperSlide key={card?.toLocaleString()}>{card}</SwiperSlide>
-                    ))}
-                </Swiper>
-            </Box>
-        </Container>
-    );
+  console.log(slider);
+
+  return (
+    <Container sx={wrapperBoxSx}>
+      <Stack
+        sx={{ width: '100%' }}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Typography variant="h4">{title}</Typography>
+        <ButtonGroup>
+          <Button onClick={() => slider?.slidePrev()}>
+            <ArrowBackIosIcon />
+          </Button>
+          <Button onClick={() => slider?.slideNext()}>
+            <ArrowForwardIosIcon />
+          </Button>
+        </ButtonGroup>
+      </Stack>
+      <Box sx={{ width: '100%', margin: '20px 0 0 0' }}>
+        <Swiper
+          style={{
+            padding: '10px',
+            width: '100%',
+            height: `${cardHeight * rows + spaceBetween * (rows)}px`,
+          }}
+          spaceBetween={spaceBetween}
+          slidesPerView={slidesPerView}
+          grid={{
+            rows: rows,
+          }}
+          onSwiper={setSlider}
+          modules={[Grid]}
+          className="mySwiper"
+        >
+          {cardsList.map(card => (
+            <SwiperSlide key={card.id} style={{ height: `${cardHeight}px` }}>
+              {card.element}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+    </Container>
+  );
 }
