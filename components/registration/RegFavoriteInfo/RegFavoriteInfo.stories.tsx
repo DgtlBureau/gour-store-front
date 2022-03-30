f914da5fee65f63b30c8077e1d3144b3e550ee3a
MikleBr <barulin.ma@gmail.com>
2022-03-30 12:07:55 +0300

    Закончил компонент RegFavoriteInfo

--- components/registration/RegFavoriteInfo/RegFavoriteInfo.stories.tsx
+++ components/registration/RegFavoriteInfo/RegFavoriteInfo.stories.tsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React from 'react';
 
-import {ComponentStory, Meta} from "@storybook/react";
-import {RegFavoriteInfo, RegFavoriteInfoProps} from "./RegFavoriteInfo";
+import { ComponentStory, Meta } from '@storybook/react';
+import { RegFavoriteInfo, RegFavoriteInfoProps } from './RegFavoriteInfo';
 
 export default {
-    component: RegFavoriteInfo,
-    title: "components/registration/RegFavoriteInfo",
+  component: RegFavoriteInfo,
+  title: 'components/registration/RegFavoriteInfo',
 } as Meta;
 
-const Template: ComponentStory<typeof RegFavoriteInfo> = (args: RegFavoriteInfoProps) => <RegFavoriteInfo {...args} />;
+const Template: ComponentStory<typeof RegFavoriteInfo> = (
+  args: RegFavoriteInfoProps
+) => <RegFavoriteInfo {...args} />;
 export const DefaultRegFavoriteInfo = Template.bind({});
-const props: Partial<RegFavoriteInfoProps> = {};
+const props: Partial<RegFavoriteInfoProps> = {
+  countries: [
+    {
+      image: '',
+      title: 'Россия',
+      id: 1,
+    },
+    {
+      image: '',
+      title: 'Англия',
+      id: 2,
+    },
+    {
+      image: '',
+      title: 'Испания',
+      id: 3,
+    },
+    {
+      image: '',
+      title: 'Италия',
+      id: 4,
+    },
+  ],
+  products: [
+    {
+      image:
+        'https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Мясо',
+      id: 1,
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1486297678162-eb2a19b0a32d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80',
+      title: 'Сыр',
+      id: 2,
+    },
+    {
+      image: '',
+      title: 'Паста',
+      id: 3,
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Пицца',
+      id: 4,
+    },
+    {
+      image: '',
+      title: 'Фрукты',
+      id: 5,
+    },
+  ],
+};
 
 DefaultRegFavoriteInfo.args = props;
--- components/registration/RegFavoriteInfo/RegFavoriteInfo.tsx
+++ components/registration/RegFavoriteInfo/RegFavoriteInfo.tsx
@@ -1,27 +1,125 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './RegFavoriteInfo.module.scss';
 import translations from './RegFavoriteInfo.i18n.json';
-import {useLocalTranslation} from "../../../hooks/useLocalTranslation";
+import { useLocalTranslation } from '../../../hooks/useLocalTranslation';
+import { Grid, Stack } from '@mui/material';
+import { Button } from './../../UI/Button/Button';
+import { Typography } from './../..//UI/Typography/Typography';
 
 export type RegFavoriteInfoProps = {
-    onBack(): void;
-    countries: {
-        image: string;
-        title: string;
-        id: number;
-    }[];
-    products: {
-        image: string;
-        title: string;
-        id: number;
-    }[];
-    onSubmit(info: {
-        countries: number[];
-        products: number[];
-    }): void;
+  countries: {
+    image: string;
+    title: string;
+    id: number;
+  }[];
+  products: {
+    image: string;
+    title: string;
+    id: number;
+  }[];
+  onBack(): void;
+  onSubmit(info: { countries: number[]; products: number[] }): void;
 };
 
-export function RegFavoriteInfo(props: RegFavoriteInfoProps) {
-    const {t} = useLocalTranslation(translations);
-    return <div>RegFavoriteInfo - {t('test')}</div>
+export function RegFavoriteInfo({
+  countries,
+  products,
+  onBack,
+  onSubmit,
+}: RegFavoriteInfoProps) {
+  const { t } = useLocalTranslation(translations);
+
+  const [userCountries, setUserCountries] = useState<number[]>([]);
+  const [userProducts, setUserProducts] = useState<number[]>([]);
+
+  const isIdInList = (id: number, list: number[]) => {
+    return !!list.find(item => item === id);
+  };
+
+  const handleClickCountry = (countryId: number) => {
+    const isSelected = isIdInList(countryId, userCountries);
+    if (isSelected) {
+      const newList = userCountries.filter(id => id !== countryId);
+      return setUserCountries(newList);
+    }
+    return setUserCountries(prevList => [...prevList, countryId]);
+  };
+
+  const handleClickProducts = (productId: number) => {
+    const isSelected = isIdInList(productId, userProducts);
+    if (isSelected) {
+      const newList = userProducts.filter(id => id !== productId);
+      return setUserProducts(newList);
+    }
+    return setUserProducts(prevList => [...prevList, productId]);
+  };
+
+  const handleSubmit = () => {
+    onSubmit({
+      countries: userCountries,
+      products: userProducts,
+    });
+  };
+
+  return (
+    <Grid container spacing={2} sx={{ maxWidth: '520px' }}>
+      <Grid item xs={4}>
+        <Button variant="outlined" onClick={onBack}>
+          {t('backButton')}
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="subtitle1">{t('title')}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="subtitle1">{t('countriesTitle')}</Typography>
+      </Grid>
+      <Grid item xs={12} container>
+        {countries.map(country => (
+          <Grid item xs={2} key={country.id}>
+            <div
+              className={`${s.circle} ${
+                isIdInList(country.id, userCountries) ? s.selected : ''
+              }`}
+              style={{ backgroundImage: `url(${country.image})` }}
+              onClick={() => {
+                handleClickCountry(country.id);
+              }}
+            >
+              <Typography variant="body2">{country.title}</Typography>
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="subtitle1">{t('likedTitle')}</Typography>
+      </Grid>
+      <Grid item xs={12} container>
+        {products.map(product => (
+          <Grid item xs={2} key={product.id}>
+            <div
+              className={`${s.circle} ${
+                isIdInList(product.id, userProducts) ? s.selected : ''
+              }`}
+              style={{ backgroundImage: `url(${product.image})` }}
+              onClick={() => {
+                handleClickProducts(product.id);
+              }}
+            >
+              <Typography variant="body2">{product.title}</Typography>
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          sx={{ width: '100%' }}
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          {t('endRegistration')}
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
