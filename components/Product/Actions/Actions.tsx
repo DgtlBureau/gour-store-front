b31eb06bda3596acf1f3227424f8ca5854a94066
MikleBr <barulin.ma@gmail.com>
2022-04-17 23:29:38 +0300

    Сделал компонент для добавления (и удаления) товара из корзины

---
+++ components/Product/Actions/Actions.tsx
@@ -0,0 +1,64 @@
+import React, { CSSProperties } from 'react';
+import { Button, ButtonGroup, Stack } from '@mui/material';
+import { Typography } from '../../UI/Typography/Typography';
+import { IconButton } from '../../UI/IconButton/IconButton';
+
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import RemoveIcon from '@mui/icons-material/Remove';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+export type ProductActionsProps = {
+  price: number;
+  count: number;
+  onAddToCart: () => void;
+  onRemoveFromCart: () => void;
+  onAddToFavorite: () => void;
+};
+
+const containerSx: CSSProperties = {
+  width: '100%',
+};
+
+export const ProductActions = ({
+  price,
+  count,
+  onAddToCart,
+  onRemoveFromCart,
+  onAddToFavorite,
+}: ProductActionsProps) => {
+  return (
+    <Stack sx={containerSx} direction="row" justifyContent="space-between">
+      <Stack>
+        <Typography variant="body1"> {price} /100г</Typography>
+        <Typography variant="h5">{price * count}</Typography>
+      </Stack>
+      <Stack direction="row">
+        {count === 0 && (
+          <Button onClick={onAddToCart} variant="contained">
+            В корзину
+          </Button>
+        )}
+        {count !== 0 && (
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button onClick={onRemoveFromCart}>
+              {count !== 1 ? <RemoveIcon /> : <DeleteIcon />}
+            </Button>
+            <Typography sx={{ padding: '0 20px' }} variant="h5">
+              {count * 100} г
+            </Typography>
+            <Button onClick={onAddToCart}>
+              <AddIcon />
+            </Button>
+          </ButtonGroup>
+        )}
+        <IconButton onClick={onAddToFavorite} component={'symbol'}>
+          <FavoriteIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};
