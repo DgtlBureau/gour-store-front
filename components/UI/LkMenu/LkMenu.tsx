f2c458aabaae0d55361ff81955f0924c1ce2ef03
MikleBr <barulin.ma@gmail.com>
2022-05-18 12:26:59 +0300

    Сделал компонент

--- components/UI/LkMenu/LkMenu.tsx
+++ components/UI/LkMenu/LkMenu.tsx
@@ -2,17 +2,37 @@ import React, { ReactElement } from 'react';
 import s from './LkMenu.module.scss';
 import translations from './LkMenu.i18n.json';
 import { useLocalTranslation } from '../../../hooks/useLocalTranslation';
+import { Stack } from '@mui/material';
+import { Button } from '../Button/Button';
+import { Typography } from './../Typography/Typography';
 
-export type LkMenuProps = {
-  title: string;
-  isActive: boolean;
+export type MenuItem = {
+  label: string;
+  path: string;
 };
 
-export function LkMenu({ title, isActive }: LkMenuProps) {
+export type LkMenuProps = {
+  active: string;
+  menuList: MenuItem[];
+  onItemClick: (path: string) => void;
+};
+
+export function LkMenu({ active, menuList, onItemClick }: LkMenuProps) {
   const { t } = useLocalTranslation(translations);
   return (
-    <span className={`${s.lkMenuItem} ${isActive ? s.active : ''}`}>
-      {title}
-    </span>
+    <Stack direction="row" alignItems="center" justifyContent="space-between">
+      <Typography>{active}</Typography>
+      <Stack direction="row" spacing={2}>
+        {menuList.map(link => (
+          <Button
+            key={link.path}
+            variant={link.label === active ? 'contained' : 'text'}
+            onClick={() => onItemClick(link.path)}
+          >
+            {link.label}
+          </Button>
+        ))}
+      </Stack>
+    </Stack>
   );
 }
