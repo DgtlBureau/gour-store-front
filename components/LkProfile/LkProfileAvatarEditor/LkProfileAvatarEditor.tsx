ded7a26528d8fc09dd9409de53b79e333f54dea4
MikleBr <barulin.ma@gmail.com>
2022-05-18 13:59:58 +0300

    Сделал первую версию компонента

--- components/LkProfile/LkProfileAvatarEditor/LkProfileAvatarEditor.tsx
+++ components/LkProfile/LkProfileAvatarEditor/LkProfileAvatarEditor.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import s from './LkProfileAvatarEditor.module.scss';
 import translations from './LkProfileAvatarEditor.i18n.json';
-import { useLocalTranslation } from 'hooks/useLocalTranslation';
+import { useLocalTranslation } from '../../../hooks/useLocalTranslation';
+import { Avatar, Stack } from '@mui/material';
+import { Button } from '../../UI/Button/Button';
 
 export type LkProfileAvatarEditorProps = {
-    image: string;
-    onChange(): void;
-    onClose(): void;
+  image: string;
+  onChange(): void;
+  onClose(): void;
 };
 
-export function LkProfileAvatarEditor(props: LkProfileAvatarEditorProps) {
-    const {t} = useLocalTranslation(translations);
-    return <div>LkProfileAvatarEditor - {t('citySelectMessage')}</div>
+export function LkProfileAvatarEditor({
+  image,
+  onChange,
+  onClose,
+}: LkProfileAvatarEditorProps) {
+  const { t } = useLocalTranslation(translations);
+  return (
+    <Stack spacing={2} alignItems="center">
+      <Avatar alt="Your profile" src={image} sx={{ width: 128, height: 128 }} />
+      <Button onClick={onChange}>Изменить фото</Button>
+      <Button variant="outlined" onClick={onClose}>
+        Удалить
+      </Button>
+    </Stack>
+  );
 }
