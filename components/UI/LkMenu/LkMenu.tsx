c42d7839f592fbb479bd0d11c94bdcd4b116e057
MikleBr <barulin.ma@gmail.com>
2022-05-18 12:27:22 +0300

    Revert "Сделал элемент LkMenu\"
    This reverts commit 92248834267b9aca3fc70bd56cb704eb1816a064.

--- components/UI/LkMenu/LkMenu.tsx
+++ components/UI/LkMenu/LkMenu.tsx
@@ -1,18 +1,13 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import s from './LkMenu.module.scss';
 import translations from './LkMenu.i18n.json';
-import { useLocalTranslation } from '../../../hooks/useLocalTranslation';
+import {useLocalTranslation} from "../../../hooks/useLocalTranslation";
 
 export type LkMenuProps = {
-  title: string;
-  isActive: boolean;
+    title: string;
 };
 
-export function LkMenu({ title, isActive }: LkMenuProps) {
-  const { t } = useLocalTranslation(translations);
-  return (
-    <span className={`${s.lkMenuItem} ${isActive ? s.active : ''}`}>
-      {title}
-    </span>
-  );
+export function LkMenu(props: LkMenuProps) {
+    const {t} = useLocalTranslation(translations);
+    return <div>LkMenu - {t('citySelectMessage')}</div>
 }
