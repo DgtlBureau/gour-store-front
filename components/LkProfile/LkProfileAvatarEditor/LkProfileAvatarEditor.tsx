ff59ffc85dc2e42bfe85ff25644ffa5fc583eced
MikleBr <barulin.ma@gmail.com>
2022-05-20 10:21:10 +0300

    Правки по реквесту

--- components/LkProfile/LkProfileAvatarEditor/LkProfileAvatarEditor.tsx
+++ components/LkProfile/LkProfileAvatarEditor/LkProfileAvatarEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import s from './LkProfileAvatarEditor.module.scss';
 import translations from './LkProfileAvatarEditor.i18n.json';
 import { useLocalTranslation } from '../../../hooks/useLocalTranslation';
@@ -8,29 +8,30 @@ import { Typography } from '../../UI/Typography/Typography';
 
 export type LkProfileAvatarEditorProps = {
   image: string;
-  onChange(): void;
-  onClose(): void;
+  onChange(event: ChangeEvent<HTMLInputElement>): void;
+  onRemove(): void;
 };
 
 export function LkProfileAvatarEditor({
   image,
   onChange,
-  onClose,
+  onRemove,
 }: LkProfileAvatarEditorProps) {
   const { t } = useLocalTranslation(translations);
+
   return (
     <Stack sx={{ width: '165px' }} spacing={2} alignItems="center">
       <Avatar alt="Your profile" src={image} sx={{ width: 128, height: 128 }} />
       <div className={s.photoInput}>
         <label htmlFor="profile-photo-input">
           <Typography variant="body1" color="#fff">
-            Изменить фото
+            {t('changePhoto')}
           </Typography>
         </label>
         <input id="profile-photo-input" type="file" onChange={onChange} />
       </div>
-      <Button sx={{ width: '100%' }} variant="outlined" onClick={onClose}>
-        Удалить
+      <Button sx={{ width: '100%' }} variant="outlined" onClick={onRemove}>
+        {t('delete')}
       </Button>
     </Stack>
   );
