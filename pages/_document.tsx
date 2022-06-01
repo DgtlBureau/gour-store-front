0cb2ff67cfa9970a93782f6c6b0620fc1bfcc9ea
Artyom Vorokhta <a.vorohta@newshift.ru>
2022-06-01 14:29:44 +0300

    game counter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,10 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Coming+Soon&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
