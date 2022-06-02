ba1992f08091ad213a9cd988f6b2e6a83ef52a14
Artyom Vorokhta <a.vorohta@newshift.ru>
2022-06-02 16:56:14 +0300

    without switches

--- components/Game/GameProduct/GameProduct.stories.tsx
+++ components/Game/GameProduct/GameProduct.stories.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { ComponentStory, Meta } from "@storybook/react";
 
-import {ComponentStory, Meta} from "@storybook/react";
-import {GameProduct, GameProductProps} from "./GameProduct";
+import { GameProduct, GameProductProps } from "./GameProduct";
 
 export default {
-    component: GameProduct,
-    title: "components/Game/GameProduct",
+  component: GameProduct,
+  title: "Game/Product",
 } as Meta;
 
 const Template: ComponentStory<typeof GameProduct> = (args: GameProductProps) => <GameProduct {...args} />;
 export const DefaultGameProduct = Template.bind({});
-const props: Partial<GameProductProps> = {};
+const props: Partial<GameProductProps> = {
+  isActive: true,
+  step: 1,
+  type: 'cheese',
+};
 
 DefaultGameProduct.args = props;
--- components/Game/GameProduct/GameProduct.tsx
+++ components/Game/GameProduct/GameProduct.tsx
@@ -1,54 +1,56 @@
 import React from 'react';
 import Image from 'next/image';
-import s from './GameProduct.module.scss';
+
+import { Box } from '../../UI/Box/Box';
 
 import cheese from '../../../assets/images/game/cheese.svg';
 import chicken from '../../../assets/images/game/chicken.svg';
 import sausage from '../../../assets/images/game/sausage.svg';
 import jamon from '../../../assets/images/game/jamon.svg';
 
+const sx = {
+  product: {
+    transition: 'all 0.5s ease',
+    height: 'fit-content',
+    width: 'fit-content',
+  },
+  muted: {
+    opacity: 0.5,
+  },
+};
+
+type GameProductType = 'cheese' | 'sausage' | 'jamon' | 'chicken';
 
 export type GameProductProps = {
-    type: 'cheese' | 'sausage' | 'jamon' | 'chicken';
-    isActive: boolean;
-    step: 'first' | 'second' | 'third' | 'four';
+  isActive: boolean;
+  type: GameProductType;
+  step: 1 | 2 | 3 | 4;
 };
 
-const checkItem = (props: string) => {
-    switch (props) {
-        case 'cheese':
-            return cheese;
-        case 'sausage':
-            return sausage;
-        case 'jamon':
-            return jamon;
-        case 'chicken':
-            return chicken;
-        default:
-            return '';
-    }
+const items = {
+  cheese,
+  sausage,
+  jamon,
+  chicken,
 };
 
-const checkStep = (props:string) => {
-    switch (props) {
-        case 'first':
-            return s.firstStep;
-        case 'second':
-            return s.secondStep;
-        case 'third':
-            return s.thirdStep;
-        case 'four':
-            return s.fourStep;
-        default:
-            return '';
-    }
+const stepAngles = {
+  1: 0,
+  2: 60,
+  3: 120,
+  4: 180,
 }
 
-export function GameProduct(props: GameProductProps) {
-    return <Image src={checkItem(props.type)}
-           alt="item"
-           width="100%"
-           height="100%"
-           className={(props.isActive ? s.elementActive : s.elementDisabled) + ' ' + (checkStep(props.step))}
-    />
+export function GameProduct({ isActive, type, step }: GameProductProps) {
+  return (
+    <Box 
+      sx={{ 
+        ...sx.product, 
+        ...(!isActive && sx.muted), 
+        transform: `rotate(${stepAngles[step]}deg)`,
+      }}
+    >
+      <Image src={items[type]} width="100%" height="100%" alt="" />
+    </Box>
+  );
 }
