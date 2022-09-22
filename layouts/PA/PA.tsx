f0c289dc3b9f4360865a0e5746665f036e118ee1
Artyom Vorokhta <avorohta@newshift.ru>
2022-09-22 19:08:43 +0300

    fix: main address change, address clear btn

--- layouts/PA/PA.tsx
+++ layouts/PA/PA.tsx
@@ -18,89 +18,85 @@ import { Currency } from 'types/entities/Currency';
 import { Path } from 'constants/routes';
 
 import sx from './PA.styles';
-import { useRouter } from 'next/router';
 
 export interface PALayoutProps {
   children?: ReactNode;
 }
 
 export function PALayout({ children }: PALayoutProps) {
-  const { goToGame } = useAppNavigation();
-  goToGame();
-  return null;
-  // const { goToFavorites, goToBasket, goToPersonalArea, goToReplenishment, language, pathname, changeChapter } =
-  //   useAppNavigation();
-  // const currency: Currency = 'cheeseCoin';
+  const { goToFavorites, goToBasket, goToPersonalArea, goToReplenishment, language, pathname, changeChapter } =
+    useAppNavigation();
+  const currency: Currency = 'cheeseCoin';
 
-  // const { data: cities } = useGetCityListQuery();
-  // const { data: currentUser } = useGetCurrentUserQuery();
-  // const { data: balance = 0 } = useGetCurrentBalanceQuery();
+  const { data: cities } = useGetCityListQuery();
+  const { data: currentUser } = useGetCurrentUserQuery();
+  const { data: balance = 0 } = useGetCurrentBalanceQuery();
 
-  // const [signOut] = useSignOutMutation();
-  // const [changeCity] = useChangeCurrentCityMutation();
+  const [signOut] = useSignOutMutation();
+  const [changeCity] = useChangeCurrentCityMutation();
 
-  // const { t } = useLocalTranslation(translations);
+  const { t } = useLocalTranslation(translations);
 
-  // const convertedCities =
-  //   cities?.map(city => ({
-  //     id: city.id,
-  //     name: city.name[language],
-  //   })) || [];
+  const convertedCities =
+    cities?.map(city => ({
+      id: city.id,
+      name: city.name[language],
+    })) || [];
 
-  // const count = useAppSelector(selectedProductCount);
-  // const sum = useAppSelector(selectedProductSum);
-  // const sumDiscount = useAppSelector(selectedProductDiscount);
+  const count = useAppSelector(selectedProductCount);
+  const sum = useAppSelector(selectedProductSum);
+  const sumDiscount = useAppSelector(selectedProductDiscount);
 
-  // const selectedCity = cities?.find(city => city.id === currentUser?.city.id) || cities?.[0];
+  const selectedCity = cities?.find(city => city.id === currentUser?.city.id) || cities?.[0];
 
-  // const menuList = [
-  //   {
-  //     label: t('main'),
-  //     path: `/${Path.PERSONAL_AREA}`,
-  //   },
-  //   {
-  //     label: t('orders'),
-  //     path: `/${Path.PERSONAL_AREA}/${Path.ORDERS}`,
-  //   },
-  //   {
-  //     label: t('credentials'),
-  //     path: `/${Path.PERSONAL_AREA}/${Path.CREDENTIALS}`,
-  //   },
-  //   {
-  //     label: t('addresses'),
-  //     path: `/${Path.PERSONAL_AREA}/${Path.ADDRESSES}`,
-  //   },
-  //   {
-  //     label: t('discounts'),
-  //     path: `/${Path.PERSONAL_AREA}/${Path.DISCOUNTS}`,
-  //   },
-  // ];
+  const menuList = [
+    {
+      label: t('main'),
+      path: `/${Path.PERSONAL_AREA}`,
+    },
+    {
+      label: t('credentials'),
+      path: `/${Path.PERSONAL_AREA}/${Path.CREDENTIALS}`,
+    },
+    {
+      label: t('addresses'),
+      path: `/${Path.PERSONAL_AREA}/${Path.ADDRESSES}`,
+    },
+    {
+      label: t('orders'),
+      path: `/${Path.PERSONAL_AREA}/${Path.ORDERS}`,
+    },
+    {
+      label: t('discounts'),
+      path: `/${Path.PERSONAL_AREA}/${Path.DISCOUNTS}`,
+    },
+  ];
 
-  // return (
-  //   <PrivateLayout>
-  //     <Box sx={sx.layout}>
-  //       <Header
-  //         {...contacts}
-  //         selectedCityId={selectedCity?.id || 0}
-  //         cities={convertedCities}
-  //         currency={currency}
-  //         language={language}
-  //         basketProductCount={count}
-  //         basketProductSum={sum - sumDiscount}
-  //         moneyAmount={balance}
-  //         onChangeCity={changeCity}
-  //         onClickFavorite={goToFavorites}
-  //         onClickPersonalArea={goToPersonalArea}
-  //         onClickBasket={goToBasket}
-  //         onClickReplenishment={goToReplenishment}
-  //         onClickSignout={signOut}
-  //       />
+  return (
+    <PrivateLayout>
+      <Box sx={sx.layout}>
+        <Header
+          {...contacts}
+          selectedCityId={selectedCity?.id || 0}
+          cities={convertedCities}
+          currency={currency}
+          language={language}
+          basketProductCount={count}
+          basketProductSum={sum - sumDiscount}
+          moneyAmount={balance}
+          onChangeCity={changeCity}
+          onClickFavorite={goToFavorites}
+          onClickPersonalArea={goToPersonalArea}
+          onClickBasket={goToBasket}
+          onClickReplenishment={goToReplenishment}
+          onClickSignout={signOut}
+        />
 
-  //       <Box sx={sx.content}>
-  //         <PAMenu active={pathname} menuList={menuList} onChange={changeChapter} />
-  //         {children}
-  //       </Box>
-  //     </Box>
-  //   </PrivateLayout>
-  // );
+        <Box sx={sx.content}>
+          <PAMenu active={pathname} menuList={menuList} onChange={changeChapter} />
+          {children}
+        </Box>
+      </Box>
+    </PrivateLayout>
+  );
 }
