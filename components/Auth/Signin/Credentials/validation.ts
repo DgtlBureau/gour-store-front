97598c26a929da2caae497067bd5fbe7d3b1866f
MirageN1349 <desertirpipi@gmail.com>
2022-09-28 00:21:32 +0300

    fix: fix signin field to email

--- components/Auth/Signin/Credentials/validation.ts
+++ components/Auth/Signin/Credentials/validation.ts
@@ -3,6 +3,6 @@ import * as yup from 'yup';
 
 export const getSchema = (t: Translator) =>
   yup.object().shape({
-    phone: yup.string().required(t('phoneEmpty')),
+    phone: yup.string().email(t('phoneError')).required(t('phoneEmpty')),
     password: yup.string().required(t('passwordError')),
   });
