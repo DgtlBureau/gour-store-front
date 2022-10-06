7b3a2e0e5050844bb41b03f47c95e6aefa23ef37
Artyom Vorokhta <avorohta@newshift.ru>
2022-10-06 15:02:38 +0300

    fix: code check

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -76,11 +76,11 @@ export default function SignUp() {
 
   const checkEmailCode = async (code: string) => {
     try {
-      await checkCode({ code }).unwrap();
+      const isSuccess = await checkCode({ code }).unwrap();
 
-      dispatchNotification('Код подтверждён');
+      if (isSuccess) dispatchNotification('Код подтверждён');
 
-      return Promise.resolve();
+      return isSuccess;
     } catch (error) {
       const message = getErrorMessage(error);
 
