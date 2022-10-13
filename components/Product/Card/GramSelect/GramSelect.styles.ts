5770d9ab6a0346ff11cf817dd36cc81e5d0f966a
Artyom Vorokhta <avorohta@newshift.ru>
2022-10-13 17:21:02 +0300

    feat: product card gram select

---
+++ components/Product/Card/GramSelect/GramSelect.styles.ts
@@ -0,0 +1,36 @@
+import { defaultTheme as t } from 'themes';
+
+export const sx = {
+  extender: {
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: { xs: 'fit-content', md: '84px' },
+    padding: { xs: '2px', md: '6px 12px' },
+    backgroundColor: 'background.paper',
+    borderRadius: '6px',
+    gap: '6px',
+  },
+  title: {
+    display: { xs: 'none', md: 'flex' },
+    color: t.palette.text.muted,
+  },
+  rotatedArrow: {
+    transform: 'rotate(180deg)',
+  },
+  list: {
+    position: 'absolute',
+    width: '84px',
+    maxHeight: '120px',
+    marginTop: '6px',
+    backgroundColor: 'white',
+    border: '1px solid',
+    borderColor: t.palette.secondary.main,
+    borderRadius: '6px',
+    zIndex: 100,
+    overflow: 'scroll',
+  },
+};
+
+export default sx;
