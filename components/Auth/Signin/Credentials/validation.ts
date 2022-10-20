f92bd6a757ba9371bedb23ccb95d4102357652a3
Artyom Vorokhta <avorohta@newshift.ru>
2022-10-20 15:06:00 +0300

    fix: 🐛 email cyrillic validation

--- components/Auth/Signin/Credentials/validation.ts
+++ components/Auth/Signin/Credentials/validation.ts
@@ -1,9 +1,14 @@
 import { Translator } from 'types/entities/Translator';
 
+import regexp from 'constants/regex';
 import * as yup from 'yup';
 
 export const getSchema = (t: Translator) =>
   yup.object().shape({
-    email: yup.string().email(t('phoneError')).required(t('phoneEmpty')),
+    email: yup
+      .string()
+      .email(t('emailError'))
+      .required(t('emailEmpty'))
+      .test('cyrillic letters', t('emailError'), value => !!value && !regexp.cyrillic.test(value)),
     password: yup.string().required(t('passwordError')),
   });
--- constants/regex.ts
+++ constants/regex.ts
@@ -2,9 +2,12 @@ const onlyDigits = /^\d*$/g;
 
 const onlyDigitsWithSign = /^-?\d*$/g;
 
+const cyrillic = /[а-яА-ЯёЁ]/g;
+
 const regexp = {
   onlyDigits,
   onlyDigitsWithSign,
+  cyrillic,
 };
 
 export default regexp;
