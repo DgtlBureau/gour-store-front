921374ef0b7aacbea67b261d0adb90a42b086bdf
Arslan Hudaygulyyev <kot4no@gmail.com>
2022-11-24 15:22:52 +0300

    fix: 🐛 Окончания для таймера акции (#225)
    * fix: 🐛 Окончания для таймера акции
    
    ✅ Closes: TSX-4077
    
    * feat: 🎸 отображение оставшихся секунд промоакции
    
    Co-authored-by: Sergey Smolkov <damager4425@gmail.com>

--- utils/timeUtil.ts
+++ utils/timeUtil.ts
@@ -1,4 +1,6 @@
-import { differenceInHours, formatDuration, intervalToDuration } from 'date-fns';
+import { differenceInHours, formatDuration, getDefaultOptions, intervalToDuration } from 'date-fns';
+
+import { decOfNum, ruDeclensions } from './wordUtil';
 
 export const formatSeconds = (seconds: number) => {
   const duration = intervalToDuration({ start: 0, end: seconds * 1000 });
@@ -9,14 +11,31 @@ export const formatSeconds = (seconds: number) => {
   return `${hours}:${formattedMinutes}:${formattedSeconds}`;
 };
 
+const getFormatDuration = (duration: Duration): Array<keyof Duration> => {
+  if (duration.days) {
+    return ['days', 'hours'];
+  }
+  if (duration.hours) {
+    return ['hours', 'minutes'];
+  }
+  return ['minutes', 'seconds'];
+};
+
 export const formatTimeLeft = (start: Date, end: Date, locale?: Locale) => {
   const duration = intervalToDuration({ start, end });
-  let format: string[];
-  if (duration.days) {
-    format = ['days', 'hours'];
-  } else {
-    format = ['hours', 'minutes'];
+  const format = getFormatDuration(duration);
+
+  const defaultOptions = getDefaultOptions() as { locale?: Locale };
+  const lang = locale?.code ?? defaultOptions.locale?.code ?? 'ru';
+
+  if (lang === 'ru') {
+    return format.reduce((acc, unit) => {
+      if (!duration[unit]) return acc;
+      const timePostfix = decOfNum(duration[unit] ?? 0, ruDeclensions[unit]);
+      return `${acc} ${duration[unit]} ${timePostfix}`;
+    }, '');
   }
+
   const time = formatDuration(duration, { locale, zero: true, format });
   return time;
 };
