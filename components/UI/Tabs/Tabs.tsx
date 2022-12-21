f31c5ac361658587b61a23b8bf494d6d81bf6063
Artyom Vorokhta <avorohta@newshift.ru>
2022-12-21 18:25:35 +0300

    fix: 🐛 personal area menu

--- components/UI/Tabs/Tabs.tsx
+++ components/UI/Tabs/Tabs.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 
 import { Tabs as MUITabs, SxProps, Tab } from '@mui/material';
+
+import { LinkRef } from '../Link/Link';
+
+const tabsListSx = {
+  width: '100%',
+  overflow: 'hidden',
+};
 
 const tabSx = {
   minWidth: 0,
@@ -12,15 +19,33 @@ export type TabsProps<T> = {
     value: T;
     label: string;
   }[];
+  withLink?: boolean;
   sx?: SxProps;
-  onChange: (val: T) => void;
+  onChange?: (val: T) => void;
 };
 
-export function Tabs<T = string | number>({ value, options, sx, onChange }: TabsProps<T>) {
+export function Tabs<T = string | number>({ value, options, sx, withLink, onChange }: TabsProps<T>) {
+  const changeTab = onChange ? (_e: SyntheticEvent<Element, Event>, newValue: T) => onChange(newValue) : undefined;
+
   return (
-    <MUITabs color='accent.main' value={value} onChange={(_, newValue: T) => onChange(newValue)} sx={sx}>
+    <MUITabs
+      color='accent.main'
+      value={value}
+      variant='scrollable'
+      scrollButtons='auto'
+      onChange={changeTab}
+      sx={{ ...tabsListSx, ...sx }}
+    >
       {options.map(option => (
-        <Tab sx={tabSx} label={option.label} value={option.value} key={String(option.value)} color='secondary' />
+        <Tab
+          LinkComponent={LinkRef}
+          key={String(option.value)}
+          label={option.label}
+          value={option.value}
+          href={withLink ? String(option.value) : ''}
+          color='secondary'
+          sx={tabSx}
+        />
       ))}
     </MUITabs>
   );
