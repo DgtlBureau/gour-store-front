da005b38b5a24bff722555f3dbbb5a87e1c8af7f
Enigmadie <gagen666@mail.ru>
2023-01-18 14:41:35 +0400

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,25 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                    (function(a,m,o,c,r,m){a[m]={id:"117761",hash:"78d744b05d391618671c168e7548fb9dd2cd804200d38f012f3e246ac1a880e1",locale:"ru",inline:false,setMeta:function(p: any){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1673980171';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));
+                    (function(a,m,o,c,r,m) {
+                      a[m] = {
+                        id:"117761",
+                        hash:"78d744b05d391618671c168e7548fb9dd2cd804200d38f012f3e246ac1a880e1",
+                        locale:"ru",
+                        inline:false,
+                        setMeta:function(p: any) {
+                          this.params=(this.params||[]).concat([p])
+                        }
+                      };
+                      a[o] = a[o] || function(){
+                        (a[o].q=a[o].q||[]).push(arguments)
+                      };
+                      var d=a.document, s=d.createElement('script');
+                      s.async=true;
+                      s.id=m+'_script';
+                      s.src='https://gso.amocrm.ru/js/button.js?1673980171';
+                      d.head && d.head.appendChild(s)
+                    }(window,0,'amoSocialButton',0,0,'amo_social_button'));
               `,
             }}
           />
