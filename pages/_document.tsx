4f7cd6cff24a534dc03263965d1fbe267b8c9515
Warmic <kopylovpeter0@gmail.com>
2023-05-15 15:24:34 +0400

    метрика убрал двойную загрузку

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,40 +64,21 @@ class MyDocument extends Document {
                       accurateTrackBounce:true,
                       webvisor:true
                  });
+                 ym(93547510, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      ecommerce:"dataLayer"
+                 });
+                 window.dataLayer = window.dataLayer || [];
             `,
             }}
           />
 
-          <script
-            type='text/javascript'
-            dangerouslySetInnerHTML={{
-              __html: `
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(93547510, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        ecommerce:"dataLayer"
-   });
-   window.dataLayer = window.dataLayer || [];
-`,
-            }}
-          />
-          <noscript>
-            <div>
-              <img src="https://mc.yandex.ru/watch/93547510" style={{position: 'absolute', left:'-9999px' }} alt="" />
-            </div>
-          </noscript>
-
-
           <noscript>
             <div>
               <img src='https://mc.yandex.ru/watch/92190821' style={{ position: 'absolute', left: '-9999px' }} alt='' />
+              <img src='https://mc.yandex.ru/watch/93547510' style={{ position: 'absolute', left: '-9999px' }} alt='' />
             </div>
           </noscript>
         </body>
