d54689258d0897ddb6e70abf47b58f18084b3652
Warmic <kopylovpeter0@gmail.com>
2023-05-18 17:00:53 +0400

    fixes for new pages

--- pages/Main.styles.ts
+++ pages/Main.styles.ts
@@ -6,7 +6,6 @@ export const sx = {
             xs: '60px 0 0 0',
         },
     },
-  },
   reviews: {
     margin: {
       sm: '40px 0 40px 0',
@@ -14,36 +13,37 @@ export const sx = {
     },
   },
   banner: {
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    borderRadius: '10px',
-    overflow: 'hidden',
-    height: {
-      md: '350px',
-      sm: '300px',
-      xs: '200px',
-    },
-    title: {
-        fontSize: {
-            sm: '40px',
-            xs: '24px',
-        },
-        fontFamily: 'Roboto slab',
-        fontWeight: 'bold',
-        color: 'text.secondary',
-    },
-    promoRow: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'justify-content',
-    },
-    pageInfoDescription: {
-        marginTop: {
-            xs: '20px',
-            md: '40px',
-        },
-    },
+      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+      borderRadius: '10px',
+      overflow: 'hidden',
+      height: {
+          md: '350px',
+          sm: '300px',
+          xs: '200px',
+      },
+  },
+  title: {
+      fontSize: {
+          sm: '40px',
+          xs: '24px',
+      },
+      fontFamily: 'Roboto slab',
+      fontWeight: 'bold',
+      color: 'text.secondary',
+  },
+  promoRow: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'justify-content',
+  },
+  pageInfoDescription: {
+      marginTop: {
+          xs: '20px',
+          md: '40px',
+      },
+  },
 };
 
 export default sx;
